651f33ef51f36cbfcebc06b3f898a30c75b2febb
Noah Prisament <nprisament@gmail.com>
2025-06-10 14:14:50 -0400

    starting place for writing new performance experiments

---
+++ benchmark/aMLIRMemsets.cpp
@@ -0,0 +1,37 @@
+#include <benchmark/benchmark.h>
+#include <cstddef>
+
+#include "../lib/memset.hpp"
+
+static constexpr int multiplier = 4;
+static constexpr int64_t start = 1<<7, limit = 1<<20;
+
+static void BM_Memset(benchmark::State& state) {
+  size_t size = state.range(0);
+  void * ptr = malloc(size);
+  
+  for (auto _ : state) {
+    mmst::memsetFunc(ptr, size);
+    benchmark::DoNotOptimize(ptr);
+  }
+
+  free(ptr);
+  state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(BM_Memset)->RangeMultiplier(multiplier)->Range(start, limit)->Complexity();
+
+static void BM_Loops64(benchmark::State& state) {
+  size_t size = state.range(0);
+  void * ptr = malloc(size);
+  
+  for (auto _ : state) {
+    mmst::assignLoopFunc(ptr, size);
+    benchmark::DoNotOptimize(ptr);
+  }
+
+  free(ptr);
+  state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(BM_Loops64)->RangeMultiplier(multiplier)->Range(start, limit)->Complexity();
